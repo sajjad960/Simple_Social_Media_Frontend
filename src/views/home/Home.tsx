d8d4b2d5538944e4720e8ffe3e237d1707f30c80
Sajjad <sajjadhossainsb01@gmail.com>
2024-03-23 01:01:56 +0600

    remove comment code

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,7 +1,6 @@
 import React, { Suspense } from "react";
 import { CircularProgress, Container } from "@mui/material";
 import CreatePost from "./components/CreatePost/CreatePost";
-// import Posts from "./components/Posts/Posts"
 import SimpleAppBar from "./components/SimpleAppBar";
 const Posts = React.lazy(() => import("./components/Posts/Posts"));
 
