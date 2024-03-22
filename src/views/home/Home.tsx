c7472787816a65c66334ba0c75e671536b06ce3b
Sajjad <sajjadhossainsb01@gmail.com>
2024-03-23 00:19:20 +0600

    added lazy load for post

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -1,19 +1,22 @@
-import { Container } from "@mui/material"
-import CreatePost from "./components/CreatePost/CreatePost"
-import Posts from "./components/Posts/Posts"
-import SimpleAppBar from "./components/SimpleAppBar"
+import React, { Suspense } from "react";
+import { CircularProgress, Container } from "@mui/material";
+import CreatePost from "./components/CreatePost/CreatePost";
+// import Posts from "./components/Posts/Posts"
+import SimpleAppBar from "./components/SimpleAppBar";
+const Posts = React.lazy(() => import("./components/Posts/Posts"));
 
 const Home = () => {
   return (
     <div>
-      <SimpleAppBar/>
-      <Container sx={{p: 4}}>
-      <CreatePost/>
-      <Posts/>
-
+      <SimpleAppBar />
+      <Container sx={{ p: 4 }}>
+        <CreatePost />
+        <Suspense fallback={<CircularProgress sx={{ mt: "1rem" }} />}>
+          <Posts />
+        </Suspense>{" "}
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
